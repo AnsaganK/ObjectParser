f6e0a0bd5f12f418503696b22b3c6ae642149cd8
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2021-11-30 03:41:47 +0600

    add QueryEditApi

--- app/views.py
+++ app/views.py
@@ -648,6 +648,7 @@ class PlaceDetail(APIView):
 class QueryEdit(APIView):
     def post(self, request, slug, format=None):
         query = generics.get_object_or_404(Query, slug=slug)
+        print(request.POST)
         form = QueryContentForm(request.POST, instance=query)
         if form.is_valid():
             form.save()
