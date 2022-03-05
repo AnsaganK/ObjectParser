444e28f5911d4b83a1098459af0334ee3193458a
Ansagan-Kabdolla <kabdolla00.00@mail.ru>
2022-03-05 20:13:02 +0600

    add word_ai

--- parsing/tasks.py
+++ parsing/tasks.py
@@ -90,10 +90,10 @@ def startFireFox(url=URL):
 
 def startChrome(url=URL, path=None):
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
     if path:
         driver = webdriver.Chrome(executable_path=path, options=chrome_options)
     else:
